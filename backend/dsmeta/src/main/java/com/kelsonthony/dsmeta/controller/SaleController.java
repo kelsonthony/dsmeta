4f7e7b6b63dec74a88d4e4d716cb851d3787f425
kelsonthony <k.thony@gmail.com>
2022-09-08 12:16:49 -0300

    update dsmeta project

---
+++ backend/dsmeta/src/main/java/com/kelsonthony/dsmeta/controller/SaleController.java
@@ -0,0 +1,38 @@
+package com.kelsonthony.dsmeta.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kelsonthony.dsmeta.entities.Sale;
+import com.kelsonthony.dsmeta.service.SaleService;
+import com.kelsonthony.dsmeta.service.SmsService;
+
+@RestController
+@RequestMapping(path = "/sales")
+public class SaleController {
+
+	@Autowired
+	private SaleService saleService;
+	
+	@Autowired
+	private SmsService smsService;
+	
+	@GetMapping
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate, 
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, 
+			Pageable pageable) {
+		return saleService.findSales(minDate, maxDate, pageable);
+	}
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+	}
+}
